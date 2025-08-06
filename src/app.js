16530a5c82ae5be3a95eec39574d3d51c966fccd
jubairk <jubairskv@gmail.com>
2025-08-06 23:25:35 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/", (req, res) => {
+  res.send("1st server code");
+});
+
+app.use("/test", (req, res) => {
+  res.send("2nd server code");
+});
+
+app.use("/server", (req, res) => {
+  res.send("3nd server code");
+});
+
+app.listen(7777, () => {
+  console.log("Server is running on port 7777");
+});
